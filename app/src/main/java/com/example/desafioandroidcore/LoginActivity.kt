f3800d2b0b16ca15e5097ac1e494b6fe740379f7
matheusvianna95 <souzamatheusvianna@gmail.com>
2020-11-12 19:10:32 -0300

    First commit including Splash, Login and Register activities

---
+++ app/src/main/java/com/example/desafioandroidcore/LoginActivity.kt
@@ -0,0 +1,27 @@
+package com.example.desafioandroidcore
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.desafioandroidcore.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.registerClickable.setOnClickListener {
+            startActivity(Intent(this@LoginActivity, RegisterActivity::class.java))
+            this@LoginActivity.finish()
+        }
+
+        binding.signInButton.setOnClickListener {
+            startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+            this@LoginActivity.finish()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/desafioandroidcore/RegisterActivity.kt
@@ -0,0 +1,27 @@
+package com.example.desafioandroidcore
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.desafioandroidcore.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.signInClickable.setOnClickListener {
+            startActivity(Intent(this@RegisterActivity, LoginActivity::class.java))
+            this@RegisterActivity.finish()
+        }
+
+        binding.registerButton.setOnClickListener {
+            startActivity(Intent(this@RegisterActivity, LoginActivity::class.java))
+            this@RegisterActivity.finish()
+        }
+    }
+}
